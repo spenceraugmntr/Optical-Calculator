7e689523132e2c4c8a034fa140ac69a8b3fb87fa
spenceraugmntr <71412412+spenceraugmntr@users.noreply.github.com>
2020-09-17 09:08:11 -0700

    Output IFOV value
    In addition added changes to output value units

--- GSD.py
+++ GSD.py
@@ -4,7 +4,7 @@ import math
 #input variables for calculating gsd
 Altitude = float(input("What is your altitude in meters?"))
 effective_imaging_element_size = float(input("What is your effective imaging element size in micrometers?"))
-focal_length = float(input("What is your focal length in (mm)?"))
+focal_length = float(input("What is your focal length in millimeters?"))
 
 #converting variable to meters
 effective_imaging_element_size = effective_imaging_element_size*.000001
@@ -13,7 +13,8 @@ focal_length = focal_length*.001
 #equations
 IFOV = math.atan2(effective_imaging_element_size, focal_length)
 GSD = Altitude*math.tan(IFOV)
-print("Your GSD is", GSD)
+print('Your IFOV is', IFOV, 'radians')
+print("Your GSD is", GSD,'meters')
 
 #Hypotenuse 
 plt.plot([0,GSD],[0,Altitude])
